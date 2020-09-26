9ee901c0b77cca19059b7035493fe9f853f6e5d9
Ranjith Kumar U <ranjithudayakumar3@gmail.com>
2020-09-27 00:41:09 +0530

    Data Layer added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebAPi from "spotify-web-api-js";
+
 
 function App() {
+  const [token, setToken] = useState(null);
+  const spotify = new SpotifyWebAPi();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash="";
+
+    const _token = hash.access_token;
+    if(_token){
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("user",user);
+      })
+    }
+                                        
+    console.log("Token : ",token);
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token ? (<Player />) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
